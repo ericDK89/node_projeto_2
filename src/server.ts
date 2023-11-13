cddc5234504d2ddb05131091f3d9ab7c28f85760
ericDK89 <ericthr42@gmail.com>
2023-11-13 19:30:56 -0300

    add: .env

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
+import { randomUUID } from 'crypto'
 import fastify from 'fastify'
 import { knex } from './db'
+import { env } from './env'
 
 const app = fastify()
-const PORT = 3333
 
-app.get('/hello', async (req, res) => {
-  const tables = await knex('sqlite_schema').select('*')
+app.post('/', async (req, reply) => {
+  await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Transaction test',
+      amount: 1000
+    })
 
-  return tables
+  await reply.code(201).send()
+})
+
+app.get('/', async (req, reply) => {
+  const transactions = await knex('transactions').select('*')
+
+  return await reply.code(200).send(transactions)
 })
 
 void app.listen({
-  port: PORT
+  port: env.PORT
 }).then(() => {
-  console.log(`Server listening on ${PORT}`)
+  console.log(`Server listening on ${env.PORT}`)
 })
