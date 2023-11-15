f7907388aad87ddf4fbcbefa1e8478bc90448e9c
ericDK89 <ericthr42@gmail.com>
2023-11-15 20:10:47 -0300

    add: NODE_ENV = test created

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
-import { beforeAll, describe, it, expect, afterAll } from 'vitest'
+import { beforeAll, describe, it, expect, afterAll, beforeEach } from 'vitest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -11,7 +12,12 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
-  it('should create a new transaction', async () => {
+  beforeEach(async () => {
+    execSync('npm run knex -- migrate:rollback --all')
+    execSync('npm run knex -- migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
     const newTransaction = {
       title: 'Transaction test',
       amount: 50,
@@ -23,5 +29,67 @@ describe('Transactions routes', () => {
       .send(newTransaction)
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const newTransaction = {
+      title: 'Transaction test',
+      amount: 50,
+      type: 'credit'
+    }
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(newTransaction)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listAllTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listAllTransactions.statusCode).toEqual(200)
+    expect(listAllTransactions.body.transaction).toEqual([
+      {
+        id: expect.any(String),
+        title: 'Transaction test',
+        amount: 50,
+        created_at: expect.any(String),
+        session_id: expect.any(String)
+      }
+    ])
+  })
+
+  it('should be able to list a specific transaction by id', async () => {
+    const newTransaction = {
+      title: 'Transaction test',
+      amount: 50,
+      type: 'credit'
+    }
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(newTransaction)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listAllTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const transactionId = listAllTransactions.body.transaction[0].id
+
+    const getTransactionById = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionById.body.transaction).toEqual({
+      id: expect.any(String),
+      title: 'Transaction test',
+      amount: 50,
+      created_at: expect.any(String),
+      session_id: expect.any(String)
+    })
   })
 })
