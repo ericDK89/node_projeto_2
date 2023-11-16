22bf206ff3ce214d6ec88abe18f3c11324e5298a
ericDK89 <ericthr42@gmail.com>
2023-11-15 22:19:13 -0300

    add: Change .env and database config

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,11 @@ import 'dotenv/config'
 import { knex as setupKnex, type Knex } from 'knex'
 import { env } from './env'
 
+const configDB = env.DATABASE_CLIENT === 'sqlite' ? { filename: env.DATABASE_URL } : env.DATABASE_URL
+
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  client: env.DATABASE_CLIENT,
+  connection: configDB,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from './app'
 import { env } from './env'
 
 void app.listen({
-  port: env.PORT
+  port: env.PORT,
+  host: ('RENDER' in process.env) ? '0.0.0.0' : 'localhost'
 }).then(() => {
   console.log(`Server listening on ${env.PORT}`)
 })
